6f113cb7e40e7df8146ed814f5c7b6208dcee055
Szymon Napierała <sznapierala@gmail.com>
2019-05-04 16:32:27 +0200

    kolejne cwiczenia

--- 2.py
+++ 2.py
@@ -2,9 +2,10 @@
 
 imie = input("Jak masz na imię ? ")
 print ("Cześć", imie, "!!! Miło Cię poznać")
-waga = input("Ile ważysz ? ")
-wzrost = input("Ile masz wzrotu ")
-print (imie, "Twój BMI wynosi:", int(waga) / (float(wzrost) ** 2))
+waga = int(input("Ile ważysz ? "))
+wzrost = float(input("Ile masz wzrotu "))
+BMI = waga / (wzrost ** 2)
+print (imie, "Twój BMI wynosi:", BMI)
 
 
 
---
+++ 3.py
@@ -0,0 +1,1 @@
+print("1 po angielsku: {} \n2 po angielsku: {}".format('one', 'two'))
