30ee39fe2e928a26fc4d2f2b8589f48ed43b2062
Szymon Napierała <sznapierala@gmail.com>
2019-05-04 19:00:27 +0200

    Modyfikacja 3.py

--- 3.py
+++ 3.py
@@ -1,1 +1,9 @@
-print("1 po angielsku: {} \n2 po angielsku: {}".format('one', 'two'))
+szer = 42
+print("-" * szer)
+print("|  Czas  |     Zawodnik     |    Data    |")
+print("*" * szer)
+print("| %6.3f | %16s | %10s |" % (9.58, "Usain Bolt", "16.08.2009"))
+print("| %6.3f | %16s | %10s |" % (9.69, "Tyson Gay", "20.09.2009"))
+print("| %6.3f | %16s | %10s |" % (9.69, "Yohan Blake", "23.09.2012"))
+print("| %6.3f | %16s | %10s |" % (9.74, "Asafa Powell", "2.09.2008"))
+print("-" * szer)
