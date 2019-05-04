3d223be557178845a4f43e454e23c638a3b2da82
Szymon Napierała <sznapierala@gmail.com>
2019-05-04 16:05:19 +0200

    dodatnie plikow

---
+++ 2.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+imie = input("Jak masz na imię ? ")
+print ("Cześć", imie, "!!! Miło Cię poznać")
+waga = input("Ile ważysz ? ")
+wzrost = input("Ile masz wzrotu ")
+print (imie, "Twój BMI wynosi:", int(waga) / (float(wzrost) ** 2))
+
